7d979f618f1e0b60b12862482bb6c4ec83ac1ef2
BiggusMaximus <annastyabagas164@gmail.com>
2022-07-28 17:02:18 +0700

    perbaikin codingan

--- wall_follower_PID/wall_follower_PID.ino
+++ wall_follower_PID/wall_follower_PID.ino
@@ -1,4 +1,3 @@
-//-------EDITING--------
 #include <NewPing.h>
 
 #define TRIGGER_PIN1    3      // Arduino pin tied to trigger pin on the ultrasonic sensor.
@@ -33,52 +32,48 @@ void setup()
 
 void loop() 
 {
-  //Kodingan di bawah ini untuk mengambil nilai yang terbaca oleh sensor ultrasonic (dalam satuan CM)
   delay(50);
   unsigned int us = sonar1.ping();     // Send ping, get ping time in microseconds (uS).
   int kanan = us / US_ROUNDTRIP_CM;
   unsigned int us2 = sonar2.ping();    // Send ping, get ping time in microseconds (uS).
   int depan = us2 / US_ROUNDTRIP_CM;
 
-
-  if (kanan>20) {kanan=20;}            // Pembacaan maksimal sensor kanan adalah 20, di atas 20 tetap terbaca 20.
-  else {kanan=kanan;}
-  //---akhir kodingan membaca nilai sensor---
-
-  // Kodingan di bawah ini adalah untuk melakukan aksi ketika kondisi Robot menabrak dinding di depan
-  if (depan<=4)                        // ketika sensor depan mendeteksi dinding kurang dari 3 cm robot harus belok kiri
-  {
-  digitalWrite(dir1, HIGH);            // kode untuk membuat robot berbelok ke kiri
-  digitalWrite(dir2, LOW);
-  analogWrite(pwm1, 255 - 100);
-  analogWrite(pwm2, 100);
+  if(kanan>20){ 
+    kanan=20;
+  }else {
+    kanan=kanan;
   }
-  //---akhir kodingan kondisi menabrak dinding depan---
 
-  // Kodingan di bawah ini adalah kondisi kebalikan dari kondisi di atas yaitu ketika sensor depan tidak menabrak dinding
-  // Kodingan menggunakkan pendekatan metode PID (Kontrol Proporsional).
-  else
-  {
-  int sp = 5;                          // sp = set point, posisi robot harus senantiasa di jarak 7 cm
-  int error = sp - kanan;              // mendefinisikan nilai error, selisih antara setpoint dan pembacaan sensor kanan
-    
-  digitalWrite(dir1, HIGH);            // OBSTABOT bergerak Maju
-  digitalWrite(dir2, HIGH);
-  
-  int pid = 9*error;                   // Pembobotan nilai PID, Angka 9 adalah konstanta proporsional (Kp)
-    
-  pwmA = sp*10-pid;                    // Pembobotan OUTPUT/pwm hasil perhitungan PID, saya masih belum yakin ini sesuai dengan teori PID
-  if(pwmA > Upper){pwmA = Upper;}      // Angka 10 adalah konstanta yang membuat pwm berada dikisaran angka ratusan/motor bisa berputar
-  if(pwmA < Lower){pwmA = Lower;}
-  ki = pwmA;                           //pwm kiri
-  
-  pwmB = sp*10+pid;
-  if(pwmB > Upper){pwmB = Upper;}
-  if(pwmB < Lower){pwmB = Lower;}
-  ka = pwmB;                           //pwm kanan
-      
-  analogWrite(pwm1, ka);
-  analogWrite(pwm2, ki);
+  if (depan<=4{
+    digitalWrite(dir1, HIGH);            
+    digitalWrite(dir2, LOW);
+    analogWrite(pwm1, 255 - 100);
+    analogWrite(pwm2, 100);
+  }else{
+    int sp = 5;                          
+    int error = sp - kanan;           
+    digitalWrite(dir1, HIGH);            
+    digitalWrite(dir2, HIGH);
+    int pid = 9*error;                   
+    pwmA = sp*10-pid;
+                        
+    if(pwmA > Upper){
+      pwmA = Upper;
+    }      
+    if(pwmA < Lower){
+      pwmA = Lower;
+    }
+    ki = pwmA;                           //pwm kiri
+    pwmB = sp*10+pid;
+    if(pwmB > Upper){
+      pwmB = Upper;
+    }
+    if(pwmB < Lower){
+      pwmB = Lower;
+    }
+    ka = pwmB;                           //pwm kanan  
+    analogWrite(pwm1, ka);
+    analogWrite(pwm2, ki);
   }
   Serial.println("Depan : " + String(depan) + "  | Kanan : " + String(kanan) + "  | ki : " + String(ki) + "  | ka : " + String(ka));
   delay(100);
